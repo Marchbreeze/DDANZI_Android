9be338aa1b2db5dc1dd2dc76ee3b0a8afe74ecaf
Sangho Kim <marchbreeze03@gmail.com>
2024-11-15 18:01:00 +0900

    [FIX/#153] SellRepository runCatching 제거

--- domain/src/main/kotlin/co/orange/domain/repository/SellRepository.kt
+++ domain/src/main/kotlin/co/orange/domain/repository/SellRepository.kt
@@ -11,19 +11,19 @@ import co.orange.domain.entity.response.SellRegisteredModel
 import co.orange.domain.entity.response.SignedUrlModel
 
 interface SellRepository {
-    suspend fun getSignedUrl(fileName: String): Result<SignedUrlModel>
+    suspend fun getSignedUrl(fileName: String): SignedUrlModel
 
-    suspend fun postToCheckProduct(request: SellCheckRequestModel): Result<SellCheckedProductModel>
+    suspend fun postToCheckProduct(request: SellCheckRequestModel): SellCheckedProductModel
 
-    suspend fun getProductToSell(productId: String): Result<SellProductModel>
+    suspend fun getProductToSell(productId: String): SellProductModel
 
-    suspend fun postToRegisterProduct(request: SellRegisterRequestModel): Result<SellRegisteredModel>
+    suspend fun postToRegisterProduct(request: SellRegisterRequestModel): SellRegisteredModel
 
-    suspend fun getItemDetailInfo(itemId: String): Result<SellInfoModel>
+    suspend fun getItemDetailInfo(itemId: String): SellInfoModel
 
-    suspend fun getBuyerInfo(orderId: String): Result<SellBuyerInfoModel>
+    suspend fun getBuyerInfo(orderId: String): SellBuyerInfoModel
 
-    suspend fun patchOrderConfirm(orderId: String): Result<OrderConfirmModel>
+    suspend fun patchOrderConfirm(orderId: String): OrderConfirmModel
 
-    suspend fun deleteSellingItem(itemId: String): Result<Boolean>
+    suspend fun deleteSellingItem(itemId: String): Boolean
 }
