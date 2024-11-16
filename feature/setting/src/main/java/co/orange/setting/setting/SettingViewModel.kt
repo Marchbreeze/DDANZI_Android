748a315654ba93e0a1ada3554aa4978cf0976b99
Sangho Kim <marchbreeze03@gmail.com>
2024-11-17 04:32:20 +0900

    [FEAT/#153] 설정뷰 UseCase 전환

--- feature/setting/src/main/java/co/orange/setting/setting/SettingViewModel.kt
+++ feature/setting/src/main/java/co/orange/setting/setting/SettingViewModel.kt
@@ -4,8 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import co.orange.core.state.UiState
 import co.orange.domain.entity.response.SettingInfoModel
-import co.orange.domain.repository.SettingRepository
-import co.orange.domain.repository.UserRepository
+import co.orange.domain.usecase.setting.GetUserDefaultInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -14,29 +13,27 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SettingViewModel
-    @Inject
-    constructor(
-        private val settingRepository: SettingRepository,
-        private val userRepository: UserRepository,
-    ) : ViewModel() {
-        private val _getSettingInfoState = MutableStateFlow<UiState<SettingInfoModel>>(UiState.Empty)
-        val getSettingInfoState: StateFlow<UiState<SettingInfoModel>> = _getSettingInfoState
+@Inject
+constructor(
+    private val getUserDefaultInfoUseCase: GetUserDefaultInfoUseCase
+) : ViewModel() {
+    private val _getSettingInfoState = MutableStateFlow<UiState<SettingInfoModel>>(UiState.Empty)
+    val getSettingInfoState: StateFlow<UiState<SettingInfoModel>> = _getSettingInfoState
 
-        init {
-            getSettingInfoFromServer()
-        }
+    init {
+        getSettingInfoFromServer()
+    }
 
-        private fun getSettingInfoFromServer() {
-            _getSettingInfoState.value = UiState.Loading
-            viewModelScope.launch {
-                settingRepository.getSettingInfo()
-                    .onSuccess {
-                        userRepository.setUserInfo(it.name, it.phone)
-                        _getSettingInfoState.value = UiState.Success(it)
-                    }
-                    .onFailure {
-                        _getSettingInfoState.value = UiState.Failure(it.message.toString())
-                    }
-            }
+    private fun getSettingInfoFromServer() {
+        _getSettingInfoState.value = UiState.Loading
+        viewModelScope.launch {
+            getUserDefaultInfoUseCase()
+                .onSuccess {
+                    _getSettingInfoState.value = UiState.Success(it)
+                }
+                .onFailure {
+                    _getSettingInfoState.value = UiState.Failure(it.message.toString())
+                }
         }
     }
+}
